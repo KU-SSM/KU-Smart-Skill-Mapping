b32b7b02c1b3b558baa1d4e7820c106a1073ccd2
VorakornLeechavanan <vorakorn.l@ku.th>
2026-01-26 22:43:42 +0700

    edit the login file

--- frontend/src/Components/Login.tsx
+++ frontend/src/Components/Login.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
