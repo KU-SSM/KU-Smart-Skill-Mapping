85af08a5b40d0835b49beda45b87694dc17195e8
VorakornLeechavanan <vorakorn.l@ku.th>
2026-01-26 23:07:50 +0700

    bringbackend files from main

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,11 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ARRAY
-import sqlalchemy
-
 
 class SkillMap(Base):
-    __tablename__ = 'transactions'
+    __tablename__ = 'skillmap'
 
     id = Column(Integer, primary_key=True, index=True)
-    skills = Column(ARRAY)
+    skills = Column(String) #just a placeholder for skill class for now
     category = Column(String)
     description = Column(String)
     date = Column(String)
